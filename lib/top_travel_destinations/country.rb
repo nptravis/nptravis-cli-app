f335823c4dbcd5bdb52073883d244d0036131c74
nptravis <nptravis@yahoo.com>
2018-02-12 20:40:39 +0700

    enabled scraping functionality for country names

--- lib/top_travel_destinations/country.rb
+++ lib/top_travel_destinations/country.rb
@@ -1,15 +1,25 @@
-require 'nokogiri'
-require 'pry'
-
 class TopTravelDestinations::Country
 	attr_accessor :name, :sights, :blurb, :articles
 
 	@@all = []
 
+	def initialize(name)
+		@name = name
+	end
+
 	def self.scrape_country
-		country = self.new
-		country.name = "Chile"
-		@@all << country
+		
+		doc = Nokogiri::HTML(open("https://www.lonelyplanet.com/best-in-travel/countries"))
+		
+		i = 1
+		
+		while i < 11
+			nam = doc.css("section.marketing-section article[id='#{i}'] h1").text
+			country = self.new(nam.split(".")[1].strip)
+			i+=1
+			@@all << country
+		end
+	
 	end
 
 	def self.all
